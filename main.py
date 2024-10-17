506519a1c753d9fe9e35e09b70fc32b832d8bb66
VargaDoro <varga.dorothea.timea@gmail.com>
2024-10-17 11:17:29 +0200

    Cinkelt kocka

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import feladatok
+import veletlenszamos_listak
 lista=[12, 21,56, 32, -5, -23, 35]
 
 print("1. feladat")
@@ -12,3 +13,43 @@ print(f"A neatív számok összege {ossz}")
 print("3. feladat")
 atlag:float=feladatok.ottel_oszthato(lista)
 print(f"A lista átlaga: {atlag}")
+
+print("10 érmedobás")
+elista=veletlenszamos_listak.tiz_ermedobas()
+print(f"A dobások: {elista}")
+
+print("Fej dobások")
+fej=veletlenszamos_listak.fej_dobas(elista)
+print(f"Ennyi fej dobás van {fej}")
+
+print("Kockadobások")
+klista=veletlenszamos_listak.kockadobas()
+print(f"A dobások listája: {klista}")
+
+print("Az egyesek száma")
+db=veletlenszamos_listak.szamolas(klista, 1)
+print(f"Az egyesek száma: {db}")
+
+print("A kettesek száma")
+db=veletlenszamos_listak.szamolas(klista, 2)
+print(f"A kettesek száma: {db}")
+
+print("A hármasok száma")
+db=veletlenszamos_listak.szamolas(klista, 3)
+print(f"A hármasok száma: {db}")
+
+print("A ngyesek száma")
+db=veletlenszamos_listak.szamolas(klista, 4)
+print(f"A négyesek száma: {db}")
+
+print("Az ötösök száma")
+db=veletlenszamos_listak.szamolas(klista,5)
+print(f"Az ötösök száma: {db}")
+
+print("A hatosok száma")
+db=veletlenszamos_listak.szamolas(klista, 6)
+print(f"A hatosok száma: {db}")
+
+print("A cinkelt dobások száma")
+clista=veletlenszamos_listak.cinkelt_kocka()
+print(f"A dobások: {clista}")
--- veletlenszamos_listak.py
+++ veletlenszamos_listak.py
@@ -1,14 +1,58 @@
+import random
+
 '''Készíts függvényt mely az érmedobást szimulálja s legenerál 10 db fej vagy írástés az eredményt eltárolja
  egy listában. A függvény térjen vissza a listával.
 
  A továbbiakban ezzel a listával dolgozz az alábbi függvényekben:
  számold meg hány fej dobás van a listában'''
 
+def tiz_ermedobas():
+    elista=[]
+    for i in range(0, 10, 1):
+        dobas:int = int(random.random()*2+1)
+        elista.append(dobas)
+
+    return elista
+
+def fej_dobas(elista):
+    fej:int=0
+    for i in range(0, len(elista), 1):
+        if(elista[i]<2):
+            fej+=1
+
+    return fej
 
 
+'''Készíts függvényt, mely a kockadobást szimulálja és legenerál 200bd véletlen kockadobást és az eredményt
+eltároja egy listában. A függvény térjen vissza listával'''
+def kockadobas():
+    klista=[]
+    for i in range(0, 200, 1):
+        dobas:int = int(random.random()*6+1)
+        klista.append(dobas)
 
+    return klista
 
-'''Kszíts függvénít, mely a kockadobást szimulálja és legenerál 200bd véletlen kockadobást és az eredményt
-eltároja egy listában. A függvény térjen vissza listával
+'''A továbbiakban ezze a listával dolgozz az alábbi függvényekben:
+számold meg hányszor dobtunk egyest,kettest, ... hatost!'''
 
-A továbbiakban ezze a listával dolgozz az alábbi függvényekben'''
+def szamolas(klista, szam):
+    db:int=0
+    for i in range(0, len(klista), 1):
+        if(szam==klista[i]):
+            db+=1
+
+    return db
+
+'''Készíts 'cinkelt' kockát! A hatost nagyobb valoszínűséggel dobja!
+A cinkelt kockával is futtasd le a fenti függvényeidet! Kiderűl a csalás?'''
+
+def cinkelt_kocka():
+    clista=[]
+    for i in range(0, 200, 1):
+        ckocka:int = int(random.random()*8+1)
+        if(6<ckocka):
+            ckocka==6
+            clista.append(ckocka)
+
+    return clista
